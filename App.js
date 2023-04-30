ae0dc64eac46707881aaca236f35de96b6c3e272
ashu-tosh-patel <ashutoshpatelnoida@gmail.com>
2023-04-30 17:26:45 +0530

    Made a basic reactNative app using expo that will work for both android as well as ios

--- App.js
+++ App.js
@@ -1,20 +1,43 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react'
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Button,
+  ScrollView,
+  FlatList
+} from 'react-native';
+
+import GoalInput from './components/GoalInput';
+import GoalItem from './components/GoalItem';
 
 export default function App() {
+  const [courseGoals, setCourseGoals] = useState([]);
+
+
+  const addGoalHandler = goalTitle => {
+    // console.log(enteredGoal);
+    setCourseGoals(currentGoals => [...currentGoals, { key: Math.random().toString(), value: goalTitle }]);
+
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.screen}>
+
+      <GoalInput onAddGoal={addGoalHandler} />
+      <FlatList data={courseGoals} renderItem={itemData => (
+        <GoalItem title={itemData.item.value} />
+      )}
+      />
+
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+  screen: {
+    padding: 50
+  }
 });
